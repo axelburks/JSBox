4e332cbb790101dddbb028e67d063e67555ceebf
Axel <axel.burks@gmail.com>
2017-12-24 20:56:28 +0800

    add file downloader

---
+++ File Downloader.js
@@ -0,0 +1,100 @@
+/*
+1.通过 share extension 运行，直接下载链接内容
+2.主程序内直接运行，自动填充剪贴板链接
+3.通过 url scheme 运行，可指定 auto 自动开始下载
+jsbox://run?name=File%20Downloader&url=https://dldir1.qq.com/qqfile/qq/TIM2.1.0/22747/TIM2.1.0.exe&auto=true
+
+ 脚本特点：
+
+*/
+
+$ui.render({
+  props: {
+    title: "Downloader"
+  },
+  views: [
+    {
+      type: "label",
+      props: {
+        text: "输入地址：",
+        align: $align.lefts
+      },
+      layout: function(make, view) {
+        make.left.inset(25)
+        make.top.right.inset(20)
+        make.size.equalTo($size(64, 32))
+      }
+    },
+    {
+      id: "downloadButton",
+      type: "button",
+      props: {
+        title: "下载"
+      },
+      layout: function(make) {
+        make.top.inset(265)
+        make.right.inset(25)
+        make.size.equalTo($size(64, 32))
+      },
+      events: {
+        tapped: function(sender) {
+          download()
+        }
+      }
+    },
+    {
+      id: "inputUrl",
+      type: "text",
+      props: {
+        type: $kbType.url,
+        bgcolor: $rgba(100, 100, 100, 0.2),
+        radius: 12
+      },
+      layout: function(make) {
+        make.top.equalTo($("label").bottom).offset(5)
+        make.right.left.inset(20)
+        make.size.equalTo($size(64, 192))
+      },
+      events: {
+        ready: function(sender) {
+          var auto = $context.query.auto || ($context.text ? true : false)
+          var content = $context.query.url || $context.text || ($clipboard.text ? $clipboard.text : null)
+          if(content != null){
+            sender.text = content
+          }
+          if(auto){
+            $("button").hidden = true
+            download()
+          } else {
+            sender.focus()
+          }
+        }
+        // returned: function(sender) {
+        //   download()
+        // }
+      }
+    }
+  ]
+})
+
+function download() {
+  // var url = $("text").text
+  var url = $("text").text.match(/^https?:\/\/[^\s]+/i)
+  if (url) {
+    $ui.toast("开始下载: " + url)
+    $ui.loading(true)
+    $http.download({
+        url: url,
+        progress: function (bytesWritten, totalBytes) {
+          var percentage = bytesWritten * 1.0 / totalBytes
+        },
+        handler: function(resp) {
+          $ui.loading(false)
+          $share.sheet(resp.data)
+        }
+      })
+
+  } else {
+    $ui.toast("请输入正确的下载地址格式！")
+  }
+}
--- XQRcode.js
+++ XQRcode.js
@@ -1,47 +1,47 @@
 function isContains(str, regxstr) {
-    return new RegExp(regxstr).test(str);
+    return new RegExp(regxstr).test(str)
 }
 
 function showResult(text, runningExt) {
-  //$clipboard.text = text;
-  //$clipboard.save(text);
+  //$clipboard.text = text
+  //$clipboard.save(text)
   $clipboard.set({
     "type": "public.plain-text",
     "value": text
   })
-  var scheme = text.match(/^\w+:\/\/[^\s]*/i);
+  var scheme = text.match(/^\w+:\/\/[^\s]*/i)
   if (scheme) {
-  	var url = text.match(/^(https?|weixin):\/\/[^\s]+/i);
+  	var url = text.match(/^(https?|weixin):\/\/[^\s]+/i)
   	if (url) {
   		if (isContains(url,/weibo\.cn/i)) {
-  			scheme = "weibo://qrcode";
+  			scheme = "weibo://qrcode"
   		} else if (isContains(url,/weixin:\/\/|weixin\.qq|tenpay\.com/i)) {
-        scheme = "weixin://scanqrcode";
+        scheme = "weixin://scanqrcode"
       } else if (isContains(url,/(wsk|txz)\.qq\.com/i)) {
-        scheme = "mqq://";
+        scheme = "mqq://"
       } else if (isContains(url,/zhihu\.com.*question/i)) {
-        scheme = url.toString().replace(/https?/i,"zhihu");
+        scheme = url.toString().replace(/https?/i,"zhihu")
       } else if (isContains(url,/ofo\.so\/plate/i)) {
-        scheme = "ofoapp://useBike?carno=" + url;
+        scheme = "ofoapp://useBike?carno=" + url
   		} else if (isContains(url,/taobao\.com|tb\.cn|tmall\.com|qazdsa\.com/i)) {
-        scheme = url.toString().replace(/https?/i,"taobao");
+        scheme = url.toString().replace(/https?/i,"taobao")
       } else if (isContains(url,/qr\.shouqianba\.com|qr\.alipay\.com.*PAI_LOGIN/i)) {
-          scheme = "alipays://platformapi/startapp?saId=10000007";
+          scheme = "alipays://platformapi/startapp?saId=10000007"
       } else if (isContains(url,/(qr|d)\.alipay\.com\/(kox|sux)/i)) {
-        scheme = "koubei://platformapi/startapp?saId=10000007&qrcode=" + url;
+        scheme = "koubei://platformapi/startapp?saId=10000007&qrcode=" + url
   		} else if (isContains(url,/(qr|d)\.alipay\.com|spay3\.swiftpass\.cn|tlt\.allinpay\.com|v\.ubox\.cn\/qr|i\.55tuan\.com\/rq/i)) {
-        scheme = "alipays://platformapi/startapp?saId=10000007&qrcode=" + url;
+        scheme = "alipays://platformapi/startapp?saId=10000007&qrcode=" + url
   		} else if (isContains(url,/item\.jd\.com/i)) {
-  			var skuId = url.toString().match(/item\.jd\.com\/(\d+)(?=\.html)/i)[1];
-  			scheme = "openapp.jdmobile://virtual?params=" + encodeURIComponent("{\"sourceValue\":\"0_productDetail_97\",\"des\":\"productDetail\",\"skuId\":\"" + skuId + "\",\"category\":\"jump\",\"sourceType\":\"PCUBE_CHANNEL\"}");
+  			var skuId = url.toString().match(/item\.jd\.com\/(\d+)(?=\.html)/i)[1]
+  			scheme = "openapp.jdmobile://virtual?params=" + encodeURIComponent("{\"sourceValue\":\"0_productDetail_97\",\"des\":\"productDetail\",\"skuId\":\"" + skuId + "\",\"category\":\"jump\",\"sourceType\":\"PCUBE_CHANNEL\"}")
   		} else if (isContains(url,/qr\.m\.jd\.com/i)) {
-  			var key = url.toString().match(/qr\.m\.jd\.com\/p\?k=([^\s]+)/i)[1];
-  			scheme = "openApp.jdMobile://virtual?params=" + encodeURIComponent("{\"category\":\"jump\",\"des\":\"ScanLogin\",\"key\":\"" + key + "\"}");
+  			var key = url.toString().match(/qr\.m\.jd\.com\/p\?k=([^\s]+)/i)[1]
+  			scheme = "openApp.jdMobile://virtual?params=" + encodeURIComponent("{\"category\":\"jump\",\"des\":\"ScanLogin\",\"key\":\"" + key + "\"}")
   		}
   	}
-    $app.openURL(scheme);
+    $app.openURL(scheme)
 		if (runningExt)
-			$context.close();
+			$context.close()
   } else {
     $ui.alert({
       title: "Clipboard Saved",
@@ -80,10 +80,10 @@ var qr = $context.image || ($clipboard.image ? $clipboard.image.image : null)
 if (qr == null) {
   $qrcode.scan({
     handler(string) {
-      showResult(string, false);
+      showResult(string, false)
     },
     cancelled() {
-      $app.openURL("weixin://scanqrcode");
+      $app.openURL("weixin://scanqrcode")
     }
   })
 } else {
