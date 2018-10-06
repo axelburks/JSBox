1d0898dfe56808819589a9254d3482fd2392ff4a
Axel <axelburks@users.noreply.github.com>
2018-10-06 20:35:58 +0800

    Thunder: fix for blank in widget

--- Thunder.js
+++ Thunder.js
@@ -7,35 +7,44 @@
 作者联系：https://t.me/axel_burks
 */
 
-var content = $context.query.downloadUrl || $context.link || $context.text || ($clipboard.text ? $clipboard.text : null)
-if(content != null){
-	var url = content.match(/((https?|ftp):\/\/|magnet:)[^\s]+/i)
-	var thunder_url = content.match(/thunder:\/\/[^\s]+/i)
-	if (url) {
-		url = url[0]
-		$clipboard.clear()
-		var scheme = "thunder://" + $text.base64Encode(url)
-		$app.openURL(scheme)
-		if ($context.link || $context.text) {
-			$context.close()
-		}
-	} else if(thunder_url){
-		thunder_url = thunder_url[0]
-		$app.openURL(thunder_url)
-	}else {
-		$ui.alert({
-			title: "No URLs",
-			message: content.substring(0,80),
-			actions: [{
-				title: "OK",
-				style: "Cancel",
-				handler: function() {
-					if ($context.link || $context.text) {
-						$context.close()
-					}
-					$system.home()
-				}
-			}]
-		})
-	}
+var content =
+  $context.query.downloadUrl ||
+  $context.link ||
+  $context.text ||
+  ($clipboard.text ? $clipboard.text : null);
+if (content != null) {
+  var url = content.match(/((https?|ftp):\/\/|magnet:)[^\s]+/i);
+  var thunder_url = content.match(/thunder:\/\/[^\s]+/i);
+  if (url) {
+    url = url[0];
+    $clipboard.clear();
+    var scheme = "thunder://" + $text.base64Encode(url);
+    $app.openURL(scheme);
+    if ($context.link || $context.text) {
+      $context.close();
+    }
+  } else if (thunder_url) {
+    thunder_url = thunder_url[0];
+    $app.openURL(thunder_url);
+  } else {
+    $ui.alert({
+      title: "No URLs",
+      message: content.substring(0, 80),
+      actions: [
+        {
+          title: "OK",
+          style: "Cancel",
+          handler: function() {
+            if ($context.link || $context.text) {
+              $context.close();
+            }
+            if ($app.env == $env.app) {
+              $system.home();
+            }
+            $app.close();
+          }
+        }
+      ]
+    });
+  }
 }
